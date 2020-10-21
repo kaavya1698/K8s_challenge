c308e990e20324cf811bf858109a3f77c5765e48
kaavya1698 <kaavya1698@gmail.com>
2020-10-21 16:42:46 -0400

    working model

---
+++ terraform/variables.tf
@@ -0,0 +1,81 @@
+variable control_cidr {
+  description = "CIDR for maintenance: inbound traffic will be allowed from this IPs"
+}
+
+variable default_keypair_public_key {
+  description = "Public Key of the default keypair"
+}
+
+variable default_keypair_name {
+  description = "Name of the KeyPair used for all nodes"
+  default = "k8s-not-the-hardest-way"
+}
+
+
+variable vpc_name {
+  description = "Name of the VPC"
+  default = "kubernetes"
+}
+
+variable elb_name {
+  description = "Name of the ELB for Kubernetes API"
+  default = "kubernetes"
+}
+
+variable owner {
+  default = "Kubernetes"
+}
+
+variable ansibleFilter {
+  description = "`ansibleFilter` tag value added to all instances, to enable instance filtering in Ansible dynamic inventory"
+  default = "Kubernetes01" # IF YOU CHANGE THIS YOU HAVE TO CHANGE instance_filters = tag:ansibleFilter=Kubernetes01 in ./ansible/hosts/ec2.ini
+}
+
+# Networking setup
+variable region {
+  default = "us-east-1"
+}
+
+variable zone {
+  default = "us-east-1a"
+}
+
+### VARIABLES BELOW MUST NOT BE CHANGED ###
+
+variable vpc_cidr {
+  default = "10.43.0.0/16"
+}
+
+variable kubernetes_pod_cidr {
+  default = "10.200.0.0/16"
+}
+
+
+# Instances Setup
+variable amis {
+  description = "Default AMIs to use for nodes depending on the region"
+  type = "map"
+  default = {
+
+    us-east-1 = "ami-0739f8cdb239fe9ae"
+
+  }
+}
+variable default_instance_user {
+  default = "ubuntu"
+}
+
+variable etcd_instance_type {
+  default = "t2.small"
+}
+variable controller_instance_type {
+  default = "t2.small"
+}
+variable worker_instance_type {
+  default = "t2.small"
+}
+
+
+variable kubernetes_cluster_dns {
+  default = "10.31.0.1"
+}
